ef873dd934cadd4aa3abf5518f065803d4763c96
MilianDIngco <mingco13@gmail.com>
2024-04-02 16:44:12 -0400

    Went through documentation and created basic skeleton for phase 1

--- main.c
+++ main.c
@@ -3,20 +3,37 @@
 
 int main(int argc, char** argv) {
 
+    /* store correct.txt into a 2d int array */
+
     /* while loop */
 
-    /* read logical address from file */
+    /* read logical address from file addresses.txt */
+
+    // extract page number
+
+    // extract offset value
 
     /* check TLB */
+    //increment TLB hit
 
     /* if TLB miss check page table */
 
-    /* if page fault, load */
-
+    /* if page fault, load from backing store */
+    //increment page fault
 
     /* get physical address */
 
-    
+    /* compare logical and physical address from correct array  */
+
+    /* sprintf logical address to out1.txt */
+
+    /* sprintf physical address to out2.txt */
+
+    /* sprintf byte value to out3.txt */
+
+
+    // FIGURE OUT PAGE REPLACEMENT LOL
+
 
     return 0;
 
