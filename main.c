8568b0e33acd6d7a96ad84929a0ecab8c3798e70
MilianDIngco <mingco13@gmail.com>
2024-04-02 16:32:31 -0400

    Created program structure and added files for data

--- main.c
+++ main.c
@@ -3,7 +3,21 @@
 
 int main(int argc, char** argv) {
 
+    /* while loop */
+
+    /* read logical address from file */
+
+    /* check TLB */
+
+    /* if TLB miss check page table */
+
+    /* if page fault, load */
+
+
+    /* get physical address */
+
     
 
     return 0;
+
 }
