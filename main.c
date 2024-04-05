cdcb85438c772b4bc292876b4132d8aed8a5c077
MilianDIngco <mingco13@gmail.com>
2024-04-04 23:03:06 -0400

    Updated main.c
    Added functions to interact with the available frame stack
    Updated the data structures
    Added using the page table to get the frame number
    Added partial page fault handling, still needs to load frame into memory from backing store.
    Modified page number so it gets shifted to the right 8 bits, gets in range of 0 - 256

--- main.c
+++ main.c
@@ -1,21 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-#define TLB_SIZE 15
+#define TLB_SIZE 16
 #define P_MEM_SIZE 65536
 #define PAGE_SIZE 256
 #define N_PAGE 256
 
-int TLB[TLB_SIZE][2];
-int PAGE_TABLE[N_PAGE];
+int TLB[TLB_SIZE][2];		//[PAGE #][FRAME #]
+int PAGE_TABLE[N_PAGE][2];	//[PAGE #][VALID/INVALID BIT]
 int AVAIL_FRAME[N_PAGE];
+int stack_head = N_PAGE;	
 char P_MEM[P_MEM_SIZE];
+int n_pagefault = 0;
 
 int check(int* bitmap, int pos) {
     return *bitmap & (1 << pos);
 }
 
+int pop() {
+	return AVAIL_FRAME[stack_head--];
+}
+
+void push(int frame_num) {
+	AVAIL_FRAME[++stack_head] = frame_num;
+}
+
+int is_empty() {
+	return stack_head == 0;
+}
+
 int main(int argc, char** argv) {
+
+	/* set all valid/invalid bits to false */
+	for(int i = 0; i < N_PAGE; i++) {
+		PAGE_TABLE[i][1] = 0;
+	}
+
+	/* set available frame stack*/
+	for(int i = 0; i < N_PAGE; i++) {
+		AVAIL_FRAME[i] = i;
+	}
 
     /* store correct.txt into a 2d int array */
 	int n_row = 1000;
@@ -53,7 +78,7 @@ int main(int argc, char** argv) {
     /* create stack with all available frames */
 
     /* while loop */
-	fp = fopen("addresses.txt", "rt");
+	fp = fopen(argv[1], "rt");
 	int logical_address = 0;
 	while(fscanf(fp, "%d", &logical_address) != -1) {
     /* read logical address from file addresses.txt */
@@ -69,19 +94,41 @@ int main(int argc, char** argv) {
 	// extract page number hoohoo
 		int page_num = 0;
 		for(int i = 8; i < 16; i++) {
-			page_num += check(&logical_address, i);
+			page_num += ((check(&logical_address, i) != 0) ? pow(2, i - 8) : 0);
 		}
 
     /* check TLB */
     //increment TLB hit
 
     /* if TLB miss check page table */
+	//USES INVERTED PAGE TABLE
+	// loop through page table until find page number, return index
+	int frame_num = -1;
+	for(int i = 0; i < N_PAGE; i++) {
+		if(PAGE_TABLE[i][0] == page_num && PAGE_TABLE[i][1] != 0) {
+			frame_num = i;
+			break;
+		}
+	}
 
     /* if page fault, load from backing store */
-    //increment page fault
+    if(frame_num == -1) {
+		//increment page fault
+		n_pagefault++;
+		// remove page from stack and use it
+		if(!is_empty()) {
+			frame_num = pop();
+		} else {
+			//select frame to replace using page replacement algorithm
+			frame_num = 0; 	//FOR NOW JUST REPLACES FRAME 0 
+		}
+		
+		int physical_address = frame_num << 8;
 
-    // remove page from stack and use it
+		//LOADS FRAME INTO PHYSICAL MEMORY
 
+
+	}
     /* get physical address */
 
     /* compare logical and physical address from correct array  */
